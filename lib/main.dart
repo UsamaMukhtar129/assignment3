efcfff721a110687365a48d1d94028e08309d30c
Usama Mukhtar(FA18-BCS-129) <92028293+UsamaMukhtar129@users.noreply.github.com>
2021-10-27 11:11:15 -0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/src/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,13 +10,18 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Expanded buildKey({Color color, int soundNumber}) {
+  Expanded buildKey({required Color color, required int soundNumber}) {
     return Expanded(
-      child: button(
+      child: FlatButton(
         color: color,
         onPressed: () {
+          // create a new player
+          // Audio cache is for local assets
           playSound(soundNumber);
-        }, child: null,
+        },
+        child: Text(
+          '',
+        ),
       ),
     );
   }
@@ -28,20 +34,18 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.purple, soundNumber: 7),
+            children: [
+              buildKey(color: Colors.greenAccent, soundNumber: 1),
+              buildKey(color: Colors.pink, soundNumber: 2),
+              buildKey(color: Colors.purpleAccent, soundNumber: 3),
+              buildKey(color: Colors.teal, soundNumber: 4),
+              buildKey(color: Colors.cyan, soundNumber: 5),
+              buildKey(color: Colors.purple, soundNumber: 6),
+              buildKey(color: Colors.lime, soundNumber: 7),
             ],
           ),
         ),
       ),
     );
   }
-
-  button({Color color, Null Function() onPressed, child}) {}
 }
